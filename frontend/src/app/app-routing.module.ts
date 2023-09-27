7e5951fe32134aed82c904233179f46c0b8b7009
Шторк Андрей Олегович <shtork@lanit.ru>
2023-09-28 02:21:31 +0700

    cascade

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileMatchModule } from './profile-match/profile-match.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '**', component: PageNotFoundComponent },
+  { path: '', redirectTo: 'profile-match', pathMatch: 'full' },
+  { path: 'profile-match', loadChildren: () => ProfileMatchModule }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
